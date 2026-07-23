1726bd12799e4b66a644aca6425eef5b4346b855
EddieT-TLee <adam832lee@gmail.com>
2026-07-23 15:38:18 -0500

    Updated to using stats instead of a status and a status panel shwowing off what itchis current status is

---
+++ Assets/Scripts/Itchi/Wander.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+
+public class Wander : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 2;
+    [SerializeField] private float maxWaitTime = 5;
+    
+    [Header("Itchi Refrence for Events")] [SerializeField]
+    private Stats itchi;
+
+    [Header("Itchi Needs Indicator")]
+    [SerializeField] private GameObject hungerIndicator;
+    [SerializeField] private GameObject happinessIndicator;
+
+    private Vector3 wanderTarget;
+    private float WalkTimer;
+    private float WalkTimeInterval;
+    private Camera cam;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    { 
+        cam = Camera.main;
+        PickTargetPosition();
+        WalkTimeInterval = Random.Range(3, maxWaitTime);
+        
+        // Set Indicator inactive by default
+        hungerIndicator.SetActive(false);
+        happinessIndicator.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Choosing a random position to wander to
+        transform.position = Vector3.MoveTowards(transform.position, wanderTarget, speed * Time.deltaTime);
+
+        if (WalkTimer > WalkTimeInterval)
+        {
+            WalkTimeInterval = Random.Range(3, maxWaitTime);
+            PickTargetPosition();
+            WalkTimer = 0;
+        }
+
+        WalkTimer += Time.deltaTime;
+
+    }
+    
+    
+    void OnEnable()
+    {
+        if (itchi == null)
+        {
+            Debug.LogWarning("Itchi was never assigned in Wander Scirpt");
+            return;
+        }
+
+        itchi.OnHappinessChanged += SetHappinessIndicator;
+        itchi.OnHungerChanged += SetHungerIndicator;
+    }
+
+    void OnDisable()
+    {
+        if (itchi == null)
+        {
+            Debug.LogWarning("Itchi was never assigned in Wander Scirpt");
+            return;
+        }
+
+        itchi.OnHappinessChanged -= SetHappinessIndicator;
+        itchi.OnHungerChanged -= SetHungerIndicator;
+    }
+    
+    private void PickTargetPosition()
+    {
+        float randomX = Random.Range(-0.9f, 0.9f);
+
+        transform.rotation = randomX > 0 ? Quaternion.Euler(0, 0, 0) : Quaternion.Euler(0, 180, 0);
+
+        // Change the positon to a world position
+        float halfWidth = cam.orthographicSize * cam.aspect;
+
+        float worldX = cam.transform.position.x + randomX * halfWidth;
+
+        wanderTarget = new Vector3(worldX, transform.position.y, transform.position.z);
+        // Debug.Log(wanderTarget.x);
+        // Debug.Log(randomX);
+    }
+    
+    private void SetHappinessIndicator(float current, float max)
+    {
+        if (current / max < 0.25)
+        {
+            happinessIndicator.SetActive(true);
+        }
+        else
+        {
+            happinessIndicator.SetActive(false);
+        }
+    }
+
+
+    private void SetHungerIndicator(float current, float max)
+    {
+        if (current / max < 0.25)
+        {
+            hungerIndicator.SetActive(true);
+        }
+        else
+        {
+            hungerIndicator.SetActive(false);
+        }
+    }
+    
+    
+    
+}
