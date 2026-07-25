5006d7e0ffae19502ae9fc5806505df55cdd432b
alokshr <alokshrest@gmail.com>
2026-07-24 19:01:06 -0500

    Added events to Draggable

--- Assets/Scripts/UI/Draggable/Draggable.cs
+++ Assets/Scripts/UI/Draggable/Draggable.cs
@@ -9,6 +9,11 @@ public class Draggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     private Canvas canvas;
     private RectTransform canvasRectTransform;
 
+    public event Action<PointerEventData> PointerDown;
+    public event Action<PointerEventData> BeginDrag;
+    public event Action<PointerEventData> Drag;
+    public event Action<PointerEventData> EndDrag;
+
     private void Awake()
     {
         canvas = GetComponentInParent<Canvas>();
@@ -25,18 +30,23 @@ public class Draggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
         {
             rectTransform.anchoredPosition = localPoint;
         }
+
+        PointerDown?.Invoke(eventData);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
-    {   
+    {
+        BeginDrag?.Invoke(eventData);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        Drag?.Invoke(eventData);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        EndDrag?.Invoke(eventData);
     }
 }
