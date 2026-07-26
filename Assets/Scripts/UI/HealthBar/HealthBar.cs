c977118eb11171315b3918bc173c9c6f62bbbbe8
alokshr <alokshrest@gmail.com>
2026-07-25 22:11:49 -0500

    Added face animation

--- Assets/Scripts/UI/HealthBar/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar/HealthBar.cs
@@ -3,22 +3,23 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-
+    [SerializeField] private Animator faceAnimator;
     [SerializeField] private Stats stats;
     [SerializeField] private Image fill;
 
     private void OnEnable()
     {
-        stats.OnHealthChanged += UpdateHealthBar;
+        stats.OnHealthChanged += UpdateHealthUI;
     }
 
     private void OnDisable()
     {
-        stats.OnHealthChanged -= UpdateHealthBar;
+        stats.OnHealthChanged -= UpdateHealthUI;
     }
 
-    private void UpdateHealthBar(float current, float max)
+    private void UpdateHealthUI(float current, float max)
     {
         fill.fillAmount = current/max;
+        faceAnimator.SetFloat("Health Percentage", current/max, 0.1f, Time.deltaTime);
     }
 }
